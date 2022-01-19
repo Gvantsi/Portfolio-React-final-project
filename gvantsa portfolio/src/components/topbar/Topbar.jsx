0014b8bd5ef1d94d8d044a3b849041e8eb48e4da
GVANT$A <gvantsa.beridze.2@btu.edu.ge>
2022-01-19 12:41:59 +0400

    Add files via upload

---
+++ gvantsa portfolio/src/components/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            GVANT$A
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+995 592 05 32 32</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>gvantsa.beridze.2@btu.edu.ge</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
